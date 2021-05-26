4f421b8df2de15d0e6adf010021f3df657a0f7f8
stkjoe <28199363+stkjoe@users.noreply.github.com>
2021-05-27 00:30:19 +1200

    Allow VLC to continue to the next track in queue.
    Previously not possible because of not tracking VLC state. The state is now
    trackable because a constant check is being done in another thread.

--- main.py
+++ main.py
@@ -10,3 +10,4 @@ if __name__ == "__main__":
     window.show()
 
     sys.exit(app.exec_())
+
--- package/MediaPlayer.py
+++ package/MediaPlayer.py
@@ -1,6 +1,20 @@
+from PySide6.QtCore import QObject, QThread, Signal
 import vlc
 import package.components.DatabaseAccess as DB
 import random
+import time
+
+class Worker(QObject):
+    check = Signal()
+
+    def __init__(self, mediaPlayer):
+        QObject.__init__(self)
+        self.mediaPlayer = mediaPlayer
+
+    def run(self):
+        while True:
+            time.sleep(1)
+            self.check.emit()
 
 class MediaPlayer():
     def __init__(self, songQueue, marqueeFunc, parent=None):
@@ -9,6 +23,12 @@ class MediaPlayer():
         self.songQueue = songQueue
         self.currentSong = None
         self.marqueeFunc = marqueeFunc
+        self.thread = QThread()
+        self.worker = Worker(self)
+        self.worker.moveToThread(self.thread)
+        self.worker.check.connect(self.songEndCheck)
+        self.thread.started.connect(self.worker.run)
+        self.thread.start()
 
     def restartSong(self):
         self.player.set_media(vlc.Media(self.parent.songPath + self.currentSong["song_path"]))
@@ -22,6 +42,8 @@ class MediaPlayer():
         if len(self.songQueue.getQueue()) == 0:
             return
         self.currentSong = self.songQueue.popSong()
+        if self.parent.content.__class__.__name__ == "WindowQueue":
+            self.parent.content.queueList.updateQueue()
         self.restartSong()
 
     def swapTrack(self):
@@ -39,3 +61,11 @@ class MediaPlayer():
     def start(self):
         self.currentSong = random.choice(DB.getSongTitles(""))
         self.restartSong()
+        
+    def songEndCheck(self):
+        if self.player.get_state() == 6:
+            if len(self.songQueue.getQueue()) == 0:
+                self.start()
+            else:
+                self.skipSong()
+  
